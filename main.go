ec53a05a09645373b00e09e320683034f31f4715
Hamza Louaddi <hamzalouaddi2@gmail.com>
2025-11-24 12:11:06 +0100

    app name

--- main.go
+++ main.go
@@ -19,7 +19,7 @@ func main() {
 
 	// Create application with options
 	err := wails.Run(&options.App{
-		Title:  "ovenDesktop",
+		Title:  "Fridge",
 		Width:  1024,
 		Height:  600,
 		AssetServer: &assetserver.Options{
